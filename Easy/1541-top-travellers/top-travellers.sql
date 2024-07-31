f36035f6586502abe6d171b70342104185db6d87
arnabruteforcer <30342252+arnabruteforcer@users.noreply.github.com>
2024-07-31 22:53:59 +0530

    Update top-travellers.sql

--- Easy/1541-top-travellers/top-travellers.sql
+++ Easy/1541-top-travellers/top-travellers.sql
@@ -1,3 +1,10 @@
+------------------- Complete JOIN Approach -------------------------
+SELECT u.name, COALESCE(SUM(distance),0) AS travelled_distance FROM Users u 
+LEFT OUTER JOIN Rides r ON u.id=r.user_id
+GROUP BY (u.id,name) ORDER BY travelled_distance DESC, u.name ASC
+
+
+----------------- Nested Table approach ---------------------
 WITH distance AS
   (
    SELECT user_id, SUM(distance) AS travelled_distance FROM Rides 
