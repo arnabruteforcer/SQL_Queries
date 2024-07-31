7e167e532cf9ead5aeff32cf4a816c9038502699
arnabruteforcer <30342252+arnabruteforcer@users.noreply.github.com>
2024-07-31 22:49:23 +0530

    Time: 404 ms (31.27%) | Memory: 0B (100.00%) - LeetSync

--- Easy/1541-top-travellers/top-travellers.sql
+++ Easy/1541-top-travellers/top-travellers.sql
@@ -1,3 +1,8 @@
-SELECT u.name, COALESCE(SUM(distance),0) AS travelled_distance FROM Users u 
-LEFT OUTER JOIN Rides r ON u.id=r.user_id
-GROUP BY (u.id,name) ORDER BY travelled_distance DESC, u.name ASC
+WITH distance AS
+  (
+   SELECT user_id, SUM(distance) AS travelled_distance FROM Rides 
+        GROUP BY user_id
+  )
+
+SELECT name, COALESCE(travelled_distance,0) AS travelled_distance FROM Users u LEFT OUTER JOIN distance
+ ON u.id=distance.user_id ORDER BY travelled_distance DESC, name ASC
