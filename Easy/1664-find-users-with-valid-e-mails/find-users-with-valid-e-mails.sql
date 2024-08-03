a805c30806ddbe4ab8e2a05e495db86ce0fedc5c
arnabruteforcer <30342252+arnabruteforcer@users.noreply.github.com>
2024-08-03 15:20:25 +0530

    Time: 516 ms (25.11%) | Memory: 0B (100.00%) - LeetSync

--- Easy/1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
+++ Easy/1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -1,3 +1,2 @@
 -- Write your PostgreSQL query statement below
-SELECT * FROM Users WHERE split_part(mail,'@',2)='leetcode.com'
-AND split_part(mail,'@',1) SIMILAR TO '[a-zA-Z]+[-_.\d\w]*'
+SELECT * FROM Users WHERE mail SIMILAR TO '[a-zA-Z]+[-_.\d\w]*@leetcode.com'
