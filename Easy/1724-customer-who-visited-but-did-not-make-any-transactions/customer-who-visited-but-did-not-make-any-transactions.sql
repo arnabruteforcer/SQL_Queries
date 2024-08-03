982022f8d62981e09f47d468c677c89e605b0006
arnabruteforcer <30342252+arnabruteforcer@users.noreply.github.com>
2024-08-03 16:48:51 +0530

    Update customer-who-visited-but-did-not-make-any-transactions.sql

--- Easy/1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ Easy/1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,4 +1,10 @@
+
+1) Using Joins 
 -- ON condition is filter before join whereas WHERE condition is the filter after join -------
 SELECT customer_id,COUNT(customer_id) AS count_no_trans FROM Visits v LEFT OUTER JOIN
 Transactions t ON v.visit_id=t.visit_id WHERE t.transaction_id IS NULL 
 GROUP BY customer_id
+
+2) Using Nested subqueries
+SELECT customer_id,COUNT(customer_id) AS count_no_trans FROM Visits v WHERE v.visit_id
+NOT IN (SELECT visit_id FROM Transactions) GROUP BY customer_id
