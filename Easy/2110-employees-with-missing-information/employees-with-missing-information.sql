edd9efd7a37549a9096bfda6cecd1fa63b68bc80
arnabruteforcer <30342252+arnabruteforcer@users.noreply.github.com>
2024-08-04 12:57:11 +0530

    Time: 232 ms (54.22%) | Memory: 0B (100.00%) - LeetSync

--- Easy/2110-employees-with-missing-information/employees-with-missing-information.sql
+++ Easy/2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -1,8 +1,8 @@
---1)
--- This operation uses a full outer join on the 2 tables ----
--- as there can be data mutually exclusive data among both tables 
-SELECT COALESCE(e.employee_id,s.employee_id) AS employee_id
- FROM Employees e FULL OUTER JOIN Salaries s 
-ON e.employee_id=s.employee_id 
-WHERE e.employee_id IS NULL OR s.employee_id IS NULL
+(SELECT employee_id FROM Employees 
+EXCEPT 
+SELECT employee_id FROM Salaries)
+    UNION
+(SELECT employee_id FROM Salaries 
+EXCEPT 
+SELECT employee_id FROM Employees) 
 ORDER BY employee_id
