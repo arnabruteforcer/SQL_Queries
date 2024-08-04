509f35730629aa4306ab08a088f74d81c360c980
arnabruteforcer <30342252+arnabruteforcer@users.noreply.github.com>
2024-08-04 13:29:28 +0530

    Time: 418 ms (5.70%) | Memory: 0B (100.00%) - LeetSync

--- Easy/2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ Easy/2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,5 +1,6 @@
---------------Using LEFT JOIN----------------------
-SELECT e.employee_id FROM Employees e LEFT JOIN Employees m 
-ON e.manager_id=m.employee_id WHERE e.salary<30000
-AND (e.manager_id IS NOT NULL AND m.employee_id IS NULL)
+--------------Using Subquery ----------------------
+SELECT e.employee_id FROM Employees e 
+WHERE e.salary<30000
+AND e.manager_id NOT IN 
+    (SELECT employee_id FROM Employees)
 ORDER BY e.employee_id
