4e13bcb38238fe29ba056b2975c9b4a26c756205
arnabruteforcer <30342252+arnabruteforcer@users.noreply.github.com>
2024-08-05 02:10:42 +0530

    Time: 230 ms (32.51%) | Memory: 0B (100.00%) - LeetSync

--- Easy/626-exchange-seats/exchange-seats.sql
+++ Easy/626-exchange-seats/exchange-seats.sql
@@ -1,14 +1,9 @@
--- Write your PostgreSQL query statement below
-WITH max_id AS
-(
-    SELECT MAX(id) AS maxId FROM Seat
-)
-
-SELECT s1.id AS id,s2.student FROM Seat s1
-INNER JOIN Seat s2 ON s2.id=s1.id+1 WHERE s1.id%2=1
-     UNION
-SELECT s1.id AS id,s2.student FROM Seat s1
-INNER JOIN Seat s2 ON s2.id=s1.id-1 WHERE s1.id%2=0
-     UNION
-SELECT id,student FROM Seat WHERE id=(SELECT maxId FROM max_id) AND id%2=1
-ORDER BY id          
+-- 2)
+-- Using simple case statements and altering the ID
+SELECT 
+    CASE 
+       WHEN id%2=0 THEN id-1
+       WHEN id%2=1 AND id!=0 AND id!=(SELECT MAX(id) FROM Seat) THEN id+1
+       ELSE id
+    END AS id, student
+ FROM Seat ORDER BY id   
